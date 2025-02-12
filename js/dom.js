370e3efd8843d08e1f0773da25b51708cff521a7
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2025-02-12 16:30:42 +0200

    3

--- js/dom.js
+++ js/dom.js
@@ -68,82 +68,82 @@
 
 // page.append(articleElement)
 
-const box = document.querySelector('.box')
-// const button = document.createElement('button')
-// box.append(button)
-// box.prepend(button)
-// box.after(button)
-// box.before(button)
-// const desc = document.querySelector('.desc')
-// box.insertBefore(button, desc)
-// console.log(button)
+// const box = document.querySelector('.box')
+// // const button = document.createElement('button')
+// // box.append(button)
+// // box.prepend(button)
+// // box.after(button)
+// // box.before(button)
+// // const desc = document.querySelector('.desc')
+// // box.insertBefore(button, desc)
+// // console.log(button)
+// // console.log(box)
+// // console.log(box.innerHTML)
+// // box.innerHTML = `<img src="" alt="">`;
+// // console.log(box.innerHTML)
+// // button.textContent = "Button"
 // console.log(box)
-// console.log(box.innerHTML)
-// box.innerHTML = `<img src="" alt="">`;
-// console.log(box.innerHTML)
-// button.textContent = "Button"
-console.log(box)
-const layout = `<ul class="list">
-        <li class="item-element">1</li>
-        <li class="item-element">2</li>
-        <li class="item-element">3</li>
-        <li class="item-element">4</li>
-        <li class="item-element">5</li>
-    </ul>`
+// const layout = `<ul class="list">
+//         <li class="item-element">1</li>
+//         <li class="item-element">2</li>
+//         <li class="item-element">3</li>
+//         <li class="item-element">4</li>
+//         <li class="item-element">5</li>
+//     </ul>`
 
-box.innerHTML = layout
+// box.innerHTML = layout
 
-const fruits = [
-    {
-        title: "Jack",
-        price: 4000,
-        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC_9e2isgKNLgmnfgk20O4iSTTFYJQAmKAz7lvE93jRV7IrFbg6DiuaN_tbCPQAB7VnQtd6QW_OHog6LMQ10ApmQ"
-    },
-    {
-        title: "Bread",
-        price: 2500,
-        photo: "https://upload.wikimedia.org/wikipedia/commons/5/57/Artocarpus_altilis_%28fruit%29.jpg"
-    },
-    {
-        title: "Durian",
-        price: 9000,
-        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Durian_in_black.jpg/1200px-Durian_in_black.jpg"
-    },
-    {
-        title: "Pineapple",
-        price: 500,
-        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgzDBpJZcKMC2oKTLB2zhCgncM7kN8Qj_YCVMTEoAWF5dxVa-M7xUGGA_WiUda0QiCdTSezB_AMoMdDESHMhdO9Q"
-    },
-    {
-        title: "Dragonfruit",
-        price: 1000,
-        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Pitaya_cross_section_ed2.jpg/640px-Pitaya_cross_section_ed2.jpg"
-    }
-];
+// const fruits = [
+//     {
+//         title: "Jack",
+//         price: 4000,
+//         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC_9e2isgKNLgmnfgk20O4iSTTFYJQAmKAz7lvE93jRV7IrFbg6DiuaN_tbCPQAB7VnQtd6QW_OHog6LMQ10ApmQ"
+//     },
+//     {
+//         title: "Bread",
+//         price: 2500,
+//         photo: "https://upload.wikimedia.org/wikipedia/commons/5/57/Artocarpus_altilis_%28fruit%29.jpg"
+//     },
+//     {
+//         title: "Durian",
+//         price: 9000,
+//         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Durian_in_black.jpg/1200px-Durian_in_black.jpg"
+//     },
+//     {
+//         title: "Pineapple",
+//         price: 500,
+//         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgzDBpJZcKMC2oKTLB2zhCgncM7kN8Qj_YCVMTEoAWF5dxVa-M7xUGGA_WiUda0QiCdTSezB_AMoMdDESHMhdO9Q"
+//     },
+//     {
+//         title: "Dragonfruit",
+//         price: 1000,
+//         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Pitaya_cross_section_ed2.jpg/640px-Pitaya_cross_section_ed2.jpg"
+//     }
+// ];
 
 
-const fruitElement = fruits.map((fruit) =>
-`<li class="list-item">
-<h2>${fruit.title}</h2>
-<p>${fruit.price}</p>
-<img src="${fruit.photo}" alt="${fruit.title}">
-</li>`
-).join('')
+// const fruitElement = fruits.map((fruit) =>
+// `<li class="list-item">
+// <h2>${fruit.title}</h2>
+// <p>${fruit.price}</p>
+// <img src="${fruit.photo}" alt="${fruit.title}">
+// </li>`
+// ).join('')
 
-console.log(fruitElement)
+// console.log(fruitElement)
 
-const fruitList = document.querySelector('.fruit-list')
+// const fruitList = document.querySelector('.fruit-list')
 
-fruitList.innerHTML = fruitElement
+// fruitList.innerHTML = fruitElement
 
-fruitList.insertAdjacentElement("beforeend", fruitElement)
-const fruitsList = document.querySelector("ul")
+// fruitList.insertAdjacentElement("beforeend", fruitElement)
+// const fruitsList = document.querySelector("ul")
 
-fruits.forEach(fruit => {
-    const fruitItem = document.createElement("li")
-    const fruitText = document.createElement("h2")
-    const fruitDesc = document.createElement("p")
-    const fruitImage = document.createElement("img")
-    fruitItem.append(fruitText, fruitDesc, fruitImage)
-    fruitsList.append(fruitItem)
-})
+// fruits.forEach(fruit => {
+//     const fruitItem = document.createElement("li")
+//     const fruitText = document.createElement("h2")
+//     const fruitDesc = document.createElement("p")
+//     const fruitImage = document.createElement("img")
+//     fruitItem.append(fruitText, fruitDesc, fruitImage)
+//     fruitsList.append(fruitItem)
+// })
